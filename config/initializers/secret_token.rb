d8788cd309280a0abefebfa1f7963c7c74adb030
Edgar Ortega Ramírez <edgar@Edgars-MacBook.local>
2012-11-14 17:36:30 -0500

    Created app and added statuses update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trebook::Application.config.secret_token = '311b981ebee95c60dd1596e1878878ec3b01b1b289ee9b3b1fc17b75ff7aba22ddb44a4172f37d6026e3ec56f32f53f60aef413ea651e1502cc94ec3664fcb9d'
